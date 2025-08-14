1f0beaebbbb9318614c66308baf658474ec02c99
Prathmesh Kathar <katharprathmesh@gmail.com>
2025-08-14 11:55:55 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
 }
+
+// Typing effect
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed("#typed-output", {
+        strings: ["Machine Learning Enthusiast", "Data Science Explorer","Machine Learning Enthusiast"],
+        typeSpeed: 30,
+        backSpeed: 30,
+        backDelay: 1500,
+        loop: true
+    });
+});
